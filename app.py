e8c0c7f772ef236722ce65742f4622259424f0fc
Nazwa Ade <nazwaadefebrianty@gmail.com>
2026-06-13 17:07:03 +0700

    add main app entry

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Mental Health Prediction",
-    page_icon="🧠",
-    layout="wide"
-)
+st.set_page_config(page_title="Mental Health AI", layout="wide")
 
-st.title("🧠 Mental Health Prediction System")
-st.write("Pilih menu pada sidebar untuk memulai.")
+st.title("🧠 Mental Health AI System")
+
+st.markdown("Silakan pilih menu di sidebar")
+
+st.sidebar.success("Use pages menu →")
