c7e59fc544d26148489f6d61d9cb6d1a17130ae5
Nazwa Ade <nazwaadefebrianty@gmail.com>
2026-06-15 11:50:30 +0700

    cleanup svm version

--- modules/decision_tree.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-model = joblib.load("models/decision_tree/decision_tree.joblib")
-scaler = joblib.load("models/decision_tree/scaler.joblib")
-
-def predict_dt(data):
-    scaled_data = scaler.transform([data])
-    prediction = model.predict(scaled_data)
-    return prediction[0]
--- modules/knn.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-model = joblib.load("models/knn/knn.pkl")
-scaler = joblib.load("models/knn/scaler.pkl")
-
-def predict_knn(data):
-    scaled_data = scaler.transform([data])
-    prediction = model.predict(scaled_data)
-    return prediction[0]
